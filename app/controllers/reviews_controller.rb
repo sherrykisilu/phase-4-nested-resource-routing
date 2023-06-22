4e46c7b8d643cbe8380684fc6c93c71be55203da
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-06-22 14:11:28 +0300

    compelete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,29 +1,15 @@
-class ReviewsController < ApplicationController
+class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  def index
-    reviews = Review.all
-    render json: reviews, include: :dog_house
-  end
-
   def show
-    review = Review.find(params[:id])
-    render json: review, include: :dog_house
-  end
-
-  def create
-    review = Review.create(review_params)
-    render json: review, status: :created
+    dog_house = DogHouse.find(params[:id])
+    render json: dog_house
   end
 
   private
 
   def render_not_found_response
-    render json: { error: "Review not found" }, status: :not_found
-  end
-
-  def review_params
-    params.permit(:username, :comment, :rating)
+    render json: { error: "Dog house not found" }, status: :not_found
   end
 
 end
